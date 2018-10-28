792da9a4832e9c1850b89e3723180211a9c44f2a
Marc Nuri <marc@marcnuri.com>
2018-10-28 13:40:32 +0100

    Added configurable local destinations

--- latest/conf/bin/service.d/postfix.d/20-listen-all.sh
+++ latest/conf/bin/service.d/postfix.d/20-listen-all.sh
@@ -1,3 +1,4 @@
 go-replace --mode=lineinfile --regex \
     -s '^[\s]*inet_interfaces[\s]*=.*' -r "inet_interfaces = all" \
+    -s '^[\s#]*mydestination[\s]*=' -r 'mydestination = $myhostname localhost 127.0.0.1 172.17.0.1 $MAILBOX_DOMAIN' \
     -- /etc/postfix/main.cf
